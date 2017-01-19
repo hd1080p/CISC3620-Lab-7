6d12b17e2f4161068b2bdaabc98c485f9c3a2075
rivlev <proflevitan@gmail.com>
2017-01-18 20:34:53 -0500

    Colored triangle with static model transform

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,5 +1,11 @@
 // Local Headers
 #include "glitter.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "glm/ext.hpp"          // to print vectors and matrices
 
 // System Headers
 #include <glad/glad.h>
@@ -9,34 +15,52 @@
 #include <cstdio>
 #include <cstdlib>
 
-const GLchar* vertexShaderSource =
-"#version 330 core\n"
+#define RED 1.0f, 0.0f, 0.0f            // define macros for convenience
+#define BLUE 0.0f, 0.0f, 1.0f
+#define GREEN 0.0f, 1.0f, 0.0f
 
-"layout (location = 0) in vec3 position;"
+// device coordinates
+#define LEFT -0.5f
+#define TOP 0.5f
+#define RIGHT 0.5f
+#define BOTTOM -0.5f
+#define MIDDLE 0.0f
 
-"void main() {"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);"
-"}\0"
-;
+const GLchar* vertexSource =
+"#version 150 core\n"             // glsl version
+"in vec2 position;"               // expects 2 values for position
+"in vec3 color;"                  // and 3 values for color
+"out vec3 Color;"                 // will pass color along pipeline
+"uniform mat4 model;"             // uniform = the same for all vertices
+"void main()"
+"{"
+"    Color = color;"              // just pass color along without modifying it
+"    gl_Position = model * vec4(position, 0.0, 1.0);"   // gl_Position is special variable for final position
+"}";                                                    // must be in homogeneous coordinates -- put in 0 for z and 1 for w
+// multiply by model matrix to transform
+const GLchar* fragmentSource =
+"#version 150 core\n"
+"in vec3 Color;"
+"out vec4 outColor;"
+"void main()"
+"{"
+"    outColor = vec4(Color, 1.0);"
+"}";
 
-const GLchar* fragmentShaderSource =
-"#version 330 core\n"
-
-"out vec4 color;\n"
-
-"void main() {"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
-"}\0";
-
-GLfloat vertices[] = {
-  -2.5f,  -0.5f,  0.0f,
-  0.5f,  -0.5f,  0.0f,
-  0.0f,   0.5f,  0.0f
+// vertex data
+GLfloat vertices [] = {
+  MIDDLE, TOP, RED,
+  LEFT, BOTTOM, BLUE,
+  RIGHT, BOTTOM, GREEN,
 };
 
+// callback for keyboard input
+void character_callback(GLFWwindow* window, unsigned int codepoint)
+{
+  if (codepoint == 'x') glfwSetWindowShouldClose(window, true);
+}
 
 int main(int argc, char * argv[]) {
-  
   // Load GLFW and Create a Window
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -44,80 +68,82 @@ int main(int argc, char * argv[]) {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-  auto mWindow = glfwCreateWindow(600, 800, "OpenGL", nullptr, nullptr);
-  
+  auto mWindow = glfwCreateWindow(mWidth, mHeight, "CISC3620", nullptr, nullptr);
   // Check for Valid Context
   if (mWindow == nullptr) {
     fprintf(stderr, "Failed to Create OpenGL Context");
     return EXIT_FAILURE;
   }
-  
+  glfwSetCharCallback(mWindow, character_callback);
   // Create Context and Load OpenGL Functions
   glfwMakeContextCurrent(mWindow);
   gladLoadGL();
   fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
   
-  // vertex array object tells GPU how the data in the buffer is laid out
-  GLuint VAO;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
+  // Create Vertex Array Object: this will store all the information about the vertex data that we are about to specify
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
   
-  // vertex buffer object stores large number of vertices on GPU
-  GLuint VBO; glGenBuffers(1, &VBO);    // generate buffer object and save its id
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);   // this is going to be your GL_ARRAY_BUFFER
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // copy vertex data into buffer
+  // Create a Vertex Buffer Object and copy the vertex data to it
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   
-  // this is the first (0th) vertex attribute, has 3 components, is of type GL_FLOAT, should
-  // not be normalized, each one is 3 floats apart, start at beginning of buffer (offset is 0)
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-  glEnableVertexAttribArray(0);
-  
-  glBindBuffer(GL_ARRAY_BUFFER, 0); 
-  glBindVertexArray(0);
-  
-  
-  // shaders must be dynamically compiled at runtime
+  // Create and compile the vertex shader
   GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
   glCompileShader(vertexShader);
+  // Create and compile the fragment shader
   GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
   glCompileShader(fragmentShader);
-  
-  // Link shaders
+  // Link the vertex and fragment shader into a shader program
   GLuint shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
   glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
   
-  // Clean up: shader objects no longer needed after they are linked
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  // Specify the layout of the vertex data
+  // position
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);  // attribute location, # values, value type, normalize?, stride, offset
+  // color
+  GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+  glEnableVertexAttribArray(colAttrib);
+  glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+  
+  // model matrix
+  GLint modelTransform = glGetUniformLocation(shaderProgram, "model");
+  glm::mat4 model = glm::scale(glm::vec3(0.5f,0.5f,0.0f));
   
   // Rendering Loop
   while (glfwWindowShouldClose(mWindow) == false) {
-    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(mWindow, true);
     
     // Background Fill Color
-    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClearColor(0.85f, 0.65f, 0.65f, 0.8f);
     glClear(GL_COLOR_BUFFER_BIT);
     
-    // Draw our first triangle
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-    glBindVertexArray(0);
+    // draw triangle
+    glUniformMatrix4fv(modelTransform, 1, GL_FALSE, glm::value_ptr(model));
+    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof(vertices[0]));
 
     
-    glfwSwapBuffers(mWindow);   // flip buffers and draw
-    glfwPollEvents();           // check if any events are triggered, call corresponding callbacks
-  }
+    // Flip Buffers and Draw
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }   glfwTerminate();
   
-  // Properly de-allocate all resources once they've outlived their purpose
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glfwTerminate();
+  // clean up
+  glDeleteProgram(shaderProgram);
+  glDeleteShader(fragmentShader);
+  glDeleteShader(vertexShader);
+  glDeleteBuffers(1, &vbo);
+  glDeleteVertexArrays(1, &vao);
   
   return EXIT_SUCCESS;
 }
