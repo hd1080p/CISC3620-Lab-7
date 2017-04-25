6f794f513db58654165778acc0dd81f4ae5732ca
rivlev <proflevitan@gmail.com>
2017-04-25 09:14:17 -0400

    change to repeat

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -208,8 +208,8 @@ int main(int argc, char * argv[]) {
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	// texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	
